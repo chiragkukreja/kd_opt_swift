20653018c1e3e78ae6def96891391d5f58a835d2
czhc <cedriczhchan@gmail.com>
2017-08-28 18:01:21 +0800

    Suppress warning in TestObserver

--- KDOptCocoa.playground/Sources/TestObserver.swift
+++ KDOptCocoa.playground/Sources/TestObserver.swift
@@ -6,7 +6,7 @@ import XCTest
 
 
 public class TestObserver: NSObject, XCTestObservation {
-    public func testCase(_ testCase: XCTestCase,
+    @nonobjc public func testCase(_ testCase: XCTestCase,
                   didFailWithDescription description: String,
                   inFile filePath: String?,
                   atLine lineNumber: Int) {
