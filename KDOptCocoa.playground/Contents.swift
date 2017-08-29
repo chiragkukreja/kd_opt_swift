31ab79e2d1afac5cd7121355269a2d42ca729264
czhc <cedriczhchan@gmail.com>
2017-08-29 12:55:04 +0800

    Add boilerplate tests

--- KDOptCocoa.playground/Contents.swift
+++ KDOptCocoa.playground/Contents.swift
@@ -7,18 +7,78 @@ import XCTest
 let testObserver = TestObserver()
 XCTestObservationCenter.shared().addTestObserver(testObserver)
 
-
-// XCTests sample
-class MyTestCase: XCTestCase {
-    // your code goes here
+class EntryTest: XCTestCase {
+    func testConstructor() {
+        let entry = Entry(credits: 1)
+        XCTAssertEqual(entry.credits, 1)
+    }
 }
 
-MyTestCase.defaultTestSuite().run()
 
+class CreditManagerTest: XCTestCase {
+    let manager = CreditManager()
 
-// Spry sample
-func add(_ a: Int, _ b: Int) -> Int {
-    return a + b
+    func testConstructor() {
+        XCTAssertEqual(manager.balance, 0)
+    }
+
+    func testAddChangesBalance() {
+        let entry = Entry(credits: 5)
+        let _  = manager.add(entry: entry)
+        XCTAssertEqual(manager.balance, 5)
+    }
+
+    func testAddReturnsTuple() {
+        let entry = Entry(credits: 5)
+        XCTAssertTrue(manager.add(entry: entry) == (true, 5))
+    }
+    
+    func testDeductChangesBalance() {
+        manager.balance = 11
+        let entry = Entry(credits: 5)
+        let _  = manager.deduct(entry: entry)
+        XCTAssertEqual(manager.balance, 6)
+    }
+    
+    func testDeductReturnsTuple() {
+        manager.balance = 11
+        let entry = Entry(credits: 5)
+        XCTAssertTrue(manager.deduct(entry: entry) == (true, 6))
+    }
+
+    func testDeductReturnsFalseAndCurrentBalanceForNegativeBalance(){
+        manager.balance = 5
+        let entry = Entry(credits: 6)
+        XCTAssertTrue(manager.deduct(entry: entry) == (false, 5))
+    }
+    
 }
 
-expect(add(1,2)).to(equal(4))
+
+class Entry {
+    let description: String
+    let credits: Int
+    
+    init(credits: Int, description: String = "") {
+        self.credits = credits
+        self.description = description
+    }
+}
+
+class CreditManager {
+    var balance: Int
+    
+    init(balance: Int = 0) {
+        self.balance = balance
+    }
+    
+    func add(entry: Entry) -> (result: Bool, balance: Int) {
+        return (true, 1)
+    }
+    
+    func deduct(entry: Entry) -> (result: Bool, balance: Int ){
+        return (true, 1)
+    }
+}
+EntryTest.defaultTestSuite().run()
+CreditManagerTest.defaultTestSuite().run()
